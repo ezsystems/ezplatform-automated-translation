a417254136507546c2c8d868b468c43ec65d6f90
konradoboza <konrad.oboza@ez.no>
2022-02-14 10:53:35 +0100

    Bumped PHP requirement to 8

--- tests/lib/EncoderTest.php
+++ tests/lib/EncoderTest.php
@@ -24,7 +24,7 @@ class EncoderTest extends TestCase
     /** @var \PHPUnit_Framework_MockObject_MockObject|\eZ\Publish\Core\MVC\ConfigResolverInterface */
     private $configResolver;
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
