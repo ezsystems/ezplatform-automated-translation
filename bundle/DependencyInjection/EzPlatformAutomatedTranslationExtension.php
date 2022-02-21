d45eaeb477a657ac9e97ad3c3f83326d661d79a7
Damian Zabawa <damian.zabawa@ibexa.co>
2022-02-21 15:02:23 +0100

    IBX-2395:  fixed services override
    Fixed services override
    
    https://issues.ibexa.co/browse/IBX-2395

--- bundle/DependencyInjection/EzPlatformAutomatedTranslationExtension.php
+++ bundle/DependencyInjection/EzPlatformAutomatedTranslationExtension.php
@@ -23,14 +23,13 @@ class EzPlatformAutomatedTranslationExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        if (empty($config['system'])) {
+            return;
+        }
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         // always needed because of Bundle extension.
         $loader->load('services_override.yml');
-
-        if (empty($config['system'])) {
-            return;
-        }
 
         if (!$this->hasConfiguredClients($config, $container)) {
             return;
